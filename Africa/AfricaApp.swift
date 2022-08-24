2ad5493f362398c95b481b4b1aa2ffb995477932
Andres <andres.gonzalez2392@gmail.com>
2022-08-24 11:29:30 -0400

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by PDWS on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
